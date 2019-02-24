579aaf73dc939ee752d4b7c0a38c5506265f0abb
mrbradfo <mrbradfo@gmail.com>
2019-02-23 20:58:54 -0700

    Implemented getHistory function

--- Assignment2/src/cse360assign2/Calculator.java
+++ Assignment2/src/cse360assign2/Calculator.java
@@ -13,6 +13,7 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
+	private String history = "0";
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
@@ -23,18 +24,22 @@ public class Calculator {
 	}
 	
 	public void add (int value) {
+		history += " + " + value;
 		total += value;
 	}
 	
 	public void subtract (int value) {
+		history += " - " + value;
 		total -= value;
 	}
 	
 	public void multiply (int value) {
+		history += " * " + value;
 		total *= value;
 	}
 	
 	public void divide (int value) {
+		history += " / " + value;
 		if(value != 0)
 			total /= value;
 		
@@ -43,6 +48,6 @@ public class Calculator {
 	}
 	
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
