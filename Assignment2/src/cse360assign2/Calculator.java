d931c74fe29be58beba74b067d60a43b62ab67b8
mrbradfo <mrbradfo@gmail.com>
2019-02-23 20:11:26 -0700

    Initial commit

---
+++ Assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,44 @@
+/**
+ * CSE360 Assignment 2
+ * Practice with version control
+ * This program implements simple calculator functions
+ * 
+ * 
+ * @author Matt
+ * @version 1.0
+ * @since 2019-02-23
+ */
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
